8f5b1ef16f9c303c16f9585b3cb0d5aaddef1a7a
maruthi06 <kmmaruthi06@gmail.com>
2018-05-20 21:45:49 +0530

    updateed files

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     include('way2sms-api.php');
+    $uid = $_GET['uid'];
+    $pwd = $_GET['pwd'];
     $mobile = $_GET['mobile'];
     $message = $_GET['message'];
-    sendWay2SMS ( $mobile , $message);
+    sendWay2SMS ($uid, $pwd, $mobile , $message);
 ?>
